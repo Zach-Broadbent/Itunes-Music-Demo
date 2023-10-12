4403f0fe63924be321c7e3d5e4b9147e8c095022
Zach-Broadbent <zbroadbent1983@xmail.dixietech.edu>
2023-10-12 13:28:55 -0600

    Initial Commit

---
+++ Itunes Music Demo/Itunes_Music_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Itunes_Music_DemoApp.swift
+//  Itunes Music Demo
+//
+//  Created by Zach Broadbent on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Itunes_Music_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
